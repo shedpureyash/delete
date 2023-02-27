affac2f297808cc35fdd7a371311835b07ab2245
shedpureyash <shedpureyash@gmail.com>
2023-02-27 16:18:13 +0530

    second commit

---
+++ header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <style>
+      .green-box {
+    height: auto;
+    width: 100%;
+    padding: 32px;
+    background-color: #D9EEE1;
+      /* background-color: #4CAF50;
+      color: white;
+      padding: 20px;
+      border-radius: 5px;
+      width: 100%;
+      box-sizing: border-box; */
+    }
+   .yellow-box{
+    height: auto;
+    width: 100%;
+    padding: 32px;
+    background-color: #f7ec8f6e;
+   }
+   .brown-box{
+    height: auto;
+    width: 100%;
+    padding: 32px;
+    /* margin: 24px -20px; */
+    background-color: #E7E9EB;
+    border-radius: 5px;
+   }
+    </style>
+
+</head>
+<body>
+    <div class="row">
+        <div class="col-2">
+          <div id="list-example" class="list-group">
+            <a class="list-group-item list-group-item-action" href="introduction.php">Item 1</a>
+            <a class="list-group-item list-group-item-action" href="output.php">Item 2</a>
+            <a class="list-group-item list-group-item-action" href="whereto.php">Item 3</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-4">Item 4</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-1">Item 1</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-2">Item 2</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-3">Item 3</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-4">Item 4</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-1">Item 1</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-2">Item 2</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-3">Item 3</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-4">Item 4</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-1">Item 1</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-2">Item 2</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-3">Item 3</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-4">Item 4</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-1">Item 1</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-2">Item 2</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-3">Item 3</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-4">Item 4</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-1">Item 1</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-2">Item 2</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-3">Item 3</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-4">Item 4</a>
+          </div>
+        </div>
+        <div class="col-9">
